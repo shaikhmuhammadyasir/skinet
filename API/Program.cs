60f9540c786f7a1a7c8a012a31d84923b9b400c6
Muhammad Yasir Shaikh <muhammad.yasir.shaikh.84@outlook.com>
2024-10-26 22:56:41 +1100

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,31 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(x=>x.Price).HasColumnType("decimal(18,2)");
+        builder.Property(x=>x.Name).HasMaxLength(250);
+        builder.Property(x=>x.Description).HasMaxLength(500);
+    }
+}
